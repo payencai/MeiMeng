842e56209e5e22182f68b1e3a4a1f427d75d9123
NaNaling <771548229@qq.com>
2018-06-06 15:50:28 +0800

    添加SettingActivity

---
+++ app/src/main/java/com/example/meimeng/activity/SettingActivity.java
@@ -0,0 +1,20 @@
+package com.example.meimeng.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.meimeng.R;
+import com.example.meimeng.base.BaseActivity;
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getContentId() {
+        return R.layout.activity_setting;
+    }
+}
